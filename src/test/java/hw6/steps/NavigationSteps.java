f4cdb3704278ba67581ddf8cba32b7a1e8eb4e05
Veronika_Filicheva <veronika_filicheva@epam.com>
2019-02-18 12:15:12 +0300

    hw6

--- src/test/java/hw6/steps/NavigationSteps.java
+++ src/test/java/hw6/steps/NavigationSteps.java
@@ -1,9 +1,6 @@
 package hw6.steps;
 
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import enums.hw6.User;
-import pageObjects.hw6.HomePageSelenide;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -14,17 +11,6 @@ public class NavigationSteps {
     @Given("^I am on \"Home Page\"$")
     public void iOpenTheHomePage() {
         open(INDEX_PAGE_URL);
-    }
-
-    @Given("I am on the (.*) page")
-    public void iOnDifferentElementsPage(String subCategory){
-        new HomePageSelenide().openSubMenu("SERVICE");
-        new HomePageSelenide().openServiceSubMenu(subCategory);
-    }
-
-    @And("I login as user \"(.+)\"")
-    public void login(String userName) {
-        new HomePageSelenide().login(User.getUserByUserName(userName.toUpperCase()));
     }
 
 }
